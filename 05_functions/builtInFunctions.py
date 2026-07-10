a61176889c3b40681ed4f3ffddafdd1fdf3eac89
Admin <admin@Admins-MacBook-Pro.local>
2026-07-10 04:49:46 -0700

    Python Daily Commit

--- 05_functions/builtInFunctions.py
+++ 05_functions/builtInFunctions.py
@@ -5,3 +5,18 @@ def chaiFlavour(flavour="Ginger"):
 
 print(chaiFlavour.__name__)
 print(chaiFlavour.__doc__)
+
+def generateBill(chai=0, samosa=0):
+    """
+    Calculate the total bill for chai and samosa
+
+    :params chai: Number of chai cups (each cup 10 Rupess)
+    :params samosa: Number of samosa (each samosa 15 Rupees)
+    : return: (total amount, Thank you message as strings)
+    
+    """
+
+    total = chai*10 + samosa*15
+    return total, "Thank you so much for visiting chaicode.com"
+
+generateBill()
